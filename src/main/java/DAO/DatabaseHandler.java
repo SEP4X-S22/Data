cab25a86fa83bf1f801786a239f4e38f6a7eea7e
143473 <75113646+143473@users.noreply.github.com>
2022-04-28 14:38:01 +0200

    Update DatabaseHandler.java

--- src/main/java/DAO/DatabaseHandler.java
+++ src/main/java/DAO/DatabaseHandler.java
@@ -8,9 +8,11 @@ import WebAPI.Repos.UserRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @Configuration
 public class DatabaseHandler
@@ -20,7 +22,17 @@ public class DatabaseHandler
   @Bean CommandLineRunner initDatabase(ReadingRepo readingRepo, UserRepo userRepo)
   {
     return args -> {
-      log.info("Preloading " + readingRepo.save(new Reading()));
+      log.info("Preloading " + readingRepo.save(
+          new Reading(1,Reading.SensorType.CO2, 40, LocalDateTime.now())));
+      log.info("Preloading " + readingRepo.save(
+          new Reading(1,Reading.SensorType.Humidity, 50, LocalDateTime.now())));
+      log.info("Preloading " + readingRepo.save(
+          new Reading(1,Reading.SensorType.Light, 1000, LocalDateTime.now())));
+      log.info("Preloading " + readingRepo.save(
+          new Reading(1,Reading.SensorType.Temperature, 5, LocalDateTime.now())));
+      log.info("Preloading " + readingRepo.save(
+          new Reading(1,Reading.SensorType.Temperature, 10, LocalDateTime.now())));
+
       log.info("Preloading " + userRepo.save(new User(1, "Frodo Baggins", true)));
     };
   }
