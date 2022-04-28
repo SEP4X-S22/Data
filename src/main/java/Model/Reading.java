f75a2f77c6d4ff6ba0db24f76347d341c8061f55
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-04-28 11:31:08 +0200

    Maven + Reading field

---
+++ src/main/java/Model/Reading.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Reading
+{
+	private int id;
+	private int room;
+	private SensorType sensor;
+	private int value;
+	private LocalDateTime timeStamp;
+	
+	public Reading(int room, SensorType sensor, int value, LocalDateTime timeStamp)
+	{
+		this.room = room;
+		this.sensor = sensor;
+		this.value = value;
+		this.timeStamp = timeStamp;
+	}
+	
+	protected enum SensorType
+	{
+		Humidity, CO2, Light, Temperature
+	}
+}
