a0fcf86c2fb4da8e3a306f9dd656a07810a90a8a
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-20 14:02:39 +0200

    DAO cleanup

--- src/main/java/com/Apharma/sep4/DAO/ReadingDAO.java
+++ src/main/java/com/Apharma/sep4/DAO/ReadingDAO.java
@@ -10,74 +10,73 @@ import com.Apharma.sep4.WebAPI.Repos.SensorRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
 import java.util.List;
 
 @Component
 public class ReadingDAO implements Dao<Reading>
 {
-  @Autowired
-  private SensorRepo sensorRepo;
-
-  @Autowired
-  private RoomRepo roomRepo;
-
-  @Autowired
-  private ReadingRepo readingRepo;
-
-  public ReadingDAO()
-  {
-  }
-
-  public void storeNewEntry(int hum, double temp, int co2, String timestamp, String roomId)
-  {
-    Room room = new Room();
-    room.setId(roomId);
-
-    //TODO: JPA add to reading without getting all children
-   // Room room = roomRepo.getById(roomId);
-    List<SensorDTO> sensors = sensorRepo.getRoomSensors(roomId);
-
-    Reading humidity = new Reading(hum, timestamp);
-    Reading temperature = new Reading(temp, timestamp);
-    Reading co2Reading = new Reading(co2, timestamp);
-
-    for (int i = 0; i < sensors.size(); i++)
-    {
-      switch (sensors.get(i).getSensorType())
-      {
-        case Temperature:
-        {
-          System.out.println(sensors.get(i).getSensorType());
-          Sensor temper = new Sensor();
-          temper.setId(sensors.get(i).getId());
-          temper.setSensorType(sensors.get(i).getSensorType());
-          temperature.setSensor(temper);
-          temper.setRoom(room);
-        }
-        break;
-        case CO2:
-        {
-          Sensor cotwo = new Sensor();
-          cotwo.setId(sensors.get(i).getId());
-          cotwo.setSensorType(sensors.get(i).getSensorType());
-          co2Reading.setSensor(cotwo);
-          cotwo.setRoom(room);
-        }
-
-        break;
-        case Humidity:
-        {
-          System.out.println(sensors.get(i).getSensorType());
-          Sensor humi = new Sensor();
-          humi.setId(sensors.get(i).getId());
-          humi.setSensorType(sensors.get(i).getSensorType());
-          humidity.setSensor(humi);
-          humi.setRoom(room);
-        }
-        break;
-      }
-    }
-    roomRepo.save(room);
-  }
+	@Autowired
+	private SensorRepo sensorRepo;
+	
+	@Autowired
+	private RoomRepo roomRepo;
+	
+	@Autowired
+	private ReadingRepo readingRepo;
+	
+	public ReadingDAO()
+	{
+	}
+	
+	public void storeNewEntry(int hum, double temp, int co2, String timestamp, String roomId)
+	{
+		Room room = new Room();
+		room.setId(roomId);
+		
+		//TODO: JPA add to reading without getting all children
+		// Room room = roomRepo.getById(roomId);
+		List<SensorDTO> sensors = sensorRepo.getRoomSensors(roomId);
+		
+		Reading humidity = new Reading(hum, timestamp);
+		Reading temperature = new Reading(temp, timestamp);
+		Reading co2Reading = new Reading(co2, timestamp);
+		
+		for (int i = 0; i < sensors.size(); i++)
+		{
+			switch (sensors.get(i).getSensorType())
+			{
+				case Temperature:
+				{
+					System.out.println(sensors.get(i).getSensorType());
+					Sensor temper = new Sensor();
+					temper.setId(sensors.get(i).getId());
+					temper.setSensorType(sensors.get(i).getSensorType());
+					temperature.setSensor(temper);
+					temper.setRoom(room);
+				}
+				break;
+				case CO2:
+				{
+					Sensor cotwo = new Sensor();
+					cotwo.setId(sensors.get(i).getId());
+					cotwo.setSensorType(sensors.get(i).getSensorType());
+					co2Reading.setSensor(cotwo);
+					cotwo.setRoom(room);
+				}
+				
+				break;
+				case Humidity:
+				{
+					System.out.println(sensors.get(i).getSensorType());
+					Sensor humi = new Sensor();
+					humi.setId(sensors.get(i).getId());
+					humi.setSensorType(sensors.get(i).getSensorType());
+					humidity.setSensor(humi);
+					humi.setRoom(room);
+				}
+				break;
+			}
+		}
+		roomRepo.save(room);
+	}
 }
