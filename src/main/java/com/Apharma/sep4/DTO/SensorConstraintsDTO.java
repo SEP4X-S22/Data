0ac3af7a66236ffaae1c90fbc54728e570fff4aa
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-20 14:00:00 +0200

    DTO cleanup

--- src/main/java/com/Apharma/sep4/DTO/SensorConstraintsDTO.java
+++ src/main/java/com/Apharma/sep4/DTO/SensorConstraintsDTO.java
@@ -1,48 +1,45 @@
 package com.Apharma.sep4.DTO;
 
-
-
 public class SensorConstraintsDTO
 {
-  private int id;
-  private double constraintMinValue;
-  private double constraintMaxValue;
-
-  public SensorConstraintsDTO(int id, double constraintMinValue,
-      double constraintMaxValue)
-  {
-    this.id = id;
-    this.constraintMinValue = constraintMinValue;
-    this.constraintMaxValue = constraintMaxValue;
-  }
-
-  public int getId()
-  {
-    return id;
-  }
-
-  public void setId(int id)
-  {
-    this.id = id;
-  }
-
-  public double getConstraintMinValue()
-  {
-    return constraintMinValue;
-  }
-
-  public void setConstraintMinValue(double constraintMinValue)
-  {
-    this.constraintMinValue = constraintMinValue;
-  }
-
-  public double getConstraintMaxValue()
-  {
-    return constraintMaxValue;
-  }
-
-  public void setConstraintMaxValue(double constraintMaxValue)
-  {
-    this.constraintMaxValue = constraintMaxValue;
-  }
+	private int id;
+	private double constraintMinValue;
+	private double constraintMaxValue;
+	
+	public SensorConstraintsDTO(int id, double constraintMinValue, double constraintMaxValue)
+	{
+		this.id = id;
+		this.constraintMinValue = constraintMinValue;
+		this.constraintMaxValue = constraintMaxValue;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public double getConstraintMinValue()
+	{
+		return constraintMinValue;
+	}
+	
+	public void setConstraintMinValue(double constraintMinValue)
+	{
+		this.constraintMinValue = constraintMinValue;
+	}
+	
+	public double getConstraintMaxValue()
+	{
+		return constraintMaxValue;
+	}
+	
+	public void setConstraintMaxValue(double constraintMaxValue)
+	{
+		this.constraintMaxValue = constraintMaxValue;
+	}
 }
