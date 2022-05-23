f1782977885317ddac3594cef18058e76e017cd9
carrolljody <jodycarroll53@gmail.com>
2022-05-23 17:19:01 +0200

    Clean up staging

--- src/main/resources/DW_aPharma.sql
+++ src/main/resources/DW_aPharma.sql
@@ -7,42 +7,25 @@ CREATE TABLE IF NOT EXISTS stage_dim_rooms (
 
 --Create staging for Dim_Sensor
 CREATE TABLE IF NOT EXISTS stage_dim_sensors (
- S_ID SERIAL PRIMARY KEY,
- SensorId VARCHAR(10),
- sensorType VARCHAR(10),
+ SensorId INT PRIMARY KEY,
+ sensorType INT,
  minValue INT,
  maxValue INT
 );
 
---Create staging for Dim_Date
-CREATE TABLE IF NOT EXISTS stage_dim_date (
- D_ID INT NOT NULL,
- Date DATE,
- Day INT,
- Week INT,
- Month INT,
- Year INT
-);
-
-
-
 --Create staging for Fact_SensorReading
 CREATE TABLE IF NOT EXISTS stage_fact_sensor_reading (
  ReadingId SERIAL PRIMARY KEY,
- R_ID INT NOT NULL,
- S_ID INT NOT NULL,
- D_ID INT NOT NULL,
- T_ID INT NOT NULL,
+ RoomId VARCHAR(16) NOT NULL,
+ SensorId INT NOT NULL,
  readingValue INT,
  isOverMax BIT,
  isUnderMin BIT
 );
 
 --SET FOREIGN KEYS FOR STAGE_
-ALTER TABLE stage_fact_sensor_reading ADD CONSTRAINT FK_Stage_Fact_SensorReading_0 FOREIGN KEY (R_ID) REFERENCES stage_dim_rooms (R_ID);
-ALTER TABLE stage_fact_sensor_reading ADD CONSTRAINT FK_Stage_Fact_SensorReading_1 FOREIGN KEY (S_ID) REFERENCES stage_dim_Sensors (S_ID);
-ALTER TABLE stage_fact_sensor_reading ADD CONSTRAINT FK_Stage_Fact_SensorReading_2 FOREIGN KEY (D_ID) REFERENCES stage_dim_date (D_ID);
-ALTER TABLE stage_fact_sensor_reading ADD CONSTRAINT FK_Stage_Fact_SensorReading_3 FOREIGN KEY (T_ID) REFERENCES stage_dim_time (T_ID);
+ALTER TABLE stage_fact_sensor_reading ADD CONSTRAINT FK_Stage_Fact_SensorReading_0 FOREIGN KEY (RoomId) REFERENCES stage_dim_rooms (RoomId);
+ALTER TABLE stage_fact_sensor_reading ADD CONSTRAINT FK_Stage_Fact_SensorReading_1 FOREIGN KEY (SensorId) REFERENCES stage_dim_Sensors (SensorId);
 
 
 
