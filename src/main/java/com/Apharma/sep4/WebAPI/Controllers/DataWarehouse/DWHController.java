91d5247c2be1906cd0006285f186022a078d666a
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-26 11:42:49 +0200

    Revert "Removing redundant controller"
    This reverts commit a75099f9ec51ae17a44c82396f8063c6dbcb7bcb.

--- src/main/java/com/Apharma/sep4/WebAPI/Controllers/DataWarehouse/DWHController.java
+++ src/main/java/com/Apharma/sep4/WebAPI/Controllers/DataWarehouse/DWHController.java
@@ -1,43 +1,31 @@
 package com.Apharma.sep4.WebAPI.Controllers.DataWarehouse;
 
+import com.Apharma.sep4.Persistence.DAO.DataWarehouse.DataWarehouseDAO;
 import com.Apharma.sep4.Persistence.DAO.DataWarehouse.iDataWarehouseDAO;
 import com.Apharma.sep4.Persistence.DTO.ReadingDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-/**
- Controller class for handling requests to the DWH that deal with historical Readings.
- 
- @author 4X Data team
- @version 1.0 - 25.05.2022
- */
 @RestController
 public class DWHController
 {
-	private iDataWarehouseDAO warehouseDAO;
-  
-  /**
-   One argument constructor for a ReadingController.
-   
-   @param dataWarehouseDAO Implementation of iDataWarehouseDAO
-   */
-	public DWHController(iDataWarehouseDAO dataWarehouseDAO)
-	{
-		this.warehouseDAO = dataWarehouseDAO;
-	}
-	
-	@GetMapping("/historical/readings/day")
-	private ArrayList<ReadingDTO> getReadingsPerDay(@RequestParam int date, @RequestParam int sensorId)
-	{
-		return warehouseDAO.getReadingsPerDay(date, sensorId);
-	}
-	
-	@GetMapping("/historical/readings/week")
-	private ArrayList<ReadingDTO> getReadingsPerWeek(@RequestParam int week, @RequestParam int year)
-	{
-		return warehouseDAO.getReadingsPerWeek(week, year);
-	}
+  private iDataWarehouseDAO warehouseDAO;
+
+  public DWHController(iDataWarehouseDAO dataWarehouseDAO){
+      this.warehouseDAO = dataWarehouseDAO;
+  }
+
+  @GetMapping("/historical/readings/day")
+  private ArrayList<ReadingDTO> getReadingsPerDay(@RequestParam int date, @RequestParam int sensorId){
+    return warehouseDAO.getReadingsPerDay(date, sensorId);
+  }
+
+  @GetMapping("/historical/readings/week")
+  private ArrayList<ReadingDTO> getReadingsPerWeek(@RequestParam int week, @RequestParam int year){
+    return warehouseDAO.getReadingsPerWeek(week, year);
+  }
 }
