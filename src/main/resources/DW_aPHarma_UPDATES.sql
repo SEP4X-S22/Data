d7083d0a04a708a199e237ed9fb49d4af4487dfa
carrolljody <jodycarroll53@gmail.com>
2022-05-27 14:31:11 +0200

    Move Testing to a separate script

--- src/main/resources/DW_aPHarma_UPDATES.sql
+++ src/main/resources/DW_aPHarma_UPDATES.sql
@@ -158,37 +158,3 @@ WHERE fsr.reading_id IN (SELECT reading_id
 FROM "stage_aPharma".fact_sensor_reading EXCEPT SELECT reading_id
 FROM "DW_aPHarma".fact_sensor_reading);
 
---***************************       TESTING                                 *******************************
-
---Testing for dim_rooms count
-SELECT count(*) AS count_on_source_rooms
-    FROM "public".rooms;
-
-SELECT count(*) AS count_on_stage_dim_rooms
-    FROM "stage_aPharma".dim_rooms;
-
-SELECT count(*) AS count_on_DW_dim_rooms
-    FROM "DW_aPHarma".dim_rooms;
-
-
---Testing for dim_sensors count
-SELECT count(*) AS count_on_source_sensors
-    FROM "public".sensors;
-
-SELECT count(*) AS count_on_stage_dim_sensors
-    FROM "stage_aPharma".dim_sensors;
-
-SELECT count(*) AS count_on_DW_dim_sensors
-    FROM "DW_aPHarma".dim_sensors;
-
-
---Testing for fact count
-SELECT count(*) AS count_on_source_Readings
-    FROM "public".readings;
-
-SELECT count(*) AS count_on_stage_fact_reading
-    FROM "stage_aPharma".fact_sensor_reading;
-
-SELECT count(*) AS count_on_DW_fact_reading
-    FROM "DW_aPHarma".fact_sensor_reading;
-
--- src/main/resources/DW_aPharma.sql
+++ src/main/resources/DW_aPharma.sql
@@ -308,38 +308,3 @@ JOIN "DW_aPHarma".dim_rooms dr on dr.room_id = fsr.room_id;
 DROP TABLE genDate;
 DROP TABLE genTime;
 
-
---***************************       TESTING                                 *******************************
-
---Testing for dim_rooms count
-SELECT count(*) AS count_on_source_rooms
-    FROM "public".rooms;
-
-SELECT count(*) AS count_on_stage_dim_rooms
-    FROM "stage_aPharma".dim_rooms;
-
-SELECT count(*) AS count_on_DW_dim_rooms
-    FROM "DW_aPHarma".dim_rooms;
-
-
---Testing for dim_sensors count
-SELECT count(*) AS count_on_source_sensors
-    FROM "public".sensors;
-
-SELECT count(*) AS count_on_stage_dim_sensors
-    FROM "stage_aPharma".dim_sensors;
-
-SELECT count(*) AS count_on_DW_dim_sensors
-    FROM "DW_aPHarma".dim_sensors;
-
-
---Testing for fact count
-SELECT count(*) AS count_on_source_Readings
-    FROM "public".readings;
-
-SELECT count(*) AS count_on_stage_fact_reading
-    FROM "stage_aPharma".fact_sensor_reading;
-
-SELECT count(*) AS count_on_DW_fact_reading
-    FROM "DW_aPHarma".fact_sensor_reading;
-
